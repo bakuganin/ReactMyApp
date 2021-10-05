d6f4830cd1d2e6cdc0a5c5841e40700c7c6061e7
Jegor <Jegor.bakunin@ivkhk.ee>
2021-10-05 10:18:33 +0300

    Clocks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [time, setTime] = useState()
+
+  const update = () => { 
+    setInterval(() => {
+      setTime(new Date().toLocaleString())
+    }, 1000);
+    
+  }
+  useEffect(() => {
+   update()
+  }, [time])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {time}.</h2>
+  </div>
   );
 }
 
